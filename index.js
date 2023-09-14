0330be8cee5520f4d4cc009ce5df387b11ba0d3e
Alberto Sierra <Sierra.Alberto@iCloud.com>
2023-09-13 17:29:50 -0700

    completed  method lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+const record = [
+  { year: "2015", result: "W" },
+  { year: "2014", result: "N/A" },
+  { year: "2013", result: "L" },
+];
+
+const superbowlWin = (record) => {
+  const broncosWin = record.find((wYear) => wYear.result === "W");
+  if (broncosWin) {
+    return broncosWin.year;
+  }
+};
+const findWin = superbowlWin(record);
+console.log(findWin);
